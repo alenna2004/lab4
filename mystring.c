afad8a0705e6c3768030387e0615e09f455cb24d
alenna2004 <55256093+alenna2004@users.noreply.github.com>
2022-12-05 09:16:14 +0300

    Update mystring.c

--- mystring.c
+++ mystring.c
@@ -1,64 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "mystr.h"
 
 
-void *myrealloc(void *p, int  new_size){
-	void *new_p = malloc(new_size);
-	for(int i; i<new_size; i++){
-		new_p[i] = p[i];
-	}
+void *myrealloc(void *p, int new_size){
+	char *new_p = (char *)malloc(new_size);
+	mem_cpy(new_p, p, new_size);
 	free(p);
 	return new_p;
 }
 
+void *mem_cpy(void *dest, const void *src, int n){
+	char *cdest = (char*)dest;
+	char *csrc = (char*)src;
+	for (int i = 0; i<n; i++){
+		*(cdest+i)= csrc[i];
+	};
+	return (void*)cdest;
+}
 
-int strlen(char *s){
+int str_len(const char *s){
 	int len = 0;
-	while(*(*s+len)!='\0'){
+	while(*(s+len)!='\0'){
 		len+=1;
 	}
 	return len;
 }
 
 
-void strcat(char **s1, char *s2){
-	int len1 = strlen(*s1);
-	int len2 = strlen(s2);
+char *str_cat(char *dest, const char *app){
+	int len1 = str_len(dest);
+	int len2 = str_len(app);
 	int new_len = len1 + len2;
 	int i=0;
 	while (i<len2){
-		*(*s1+len1) = s2[i];
+		*(dest+len1) = app[i];
 		i+=1;
 		len1+=1;
 	}
+	*(dest +new_len) = '\0';
+	return dest;
 }
 
 
 
-char *get_line(void){
-	int n, len = 0
+char *get_line(const char *prompt){
+	printf("%s", prompt);
+	int n, len = 0;
 	int L = 80;
 	char buf[L+1];
-	int n;
-	char *ptr = (char*)malloc(siziof(char));
-	//*ptr = '\0'
+	char *ptr = (char*)malloc(sizeof(char));
+	*ptr = '\0';
 	do{
-		n = scanf("%80[^\n]%", buf)
+		n = scanf("%80[^\n]%", buf);
 		if (n<0){
 			free(ptr);
 			ptr = NULL;
 			continue;
 		}
 		if (n==0){
-			scanf("%*c")
+			scanf("%*c");
 		}
 		else{
-			len += strlen(buf);
-			ptr = (char*)realloc(ptr,(len+1)*sizeof(char));
-			strcat(&ptr, buf)
+			len += str_len(buf);
+			ptr = (char*)myrealloc(ptr,(len+1)*sizeof(char));
+			str_cat(ptr, buf);
 		}
 	}
 	while(n>0);
-	return 0;
+	return ptr;
 }
-
